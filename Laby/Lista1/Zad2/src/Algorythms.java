9d0cf5daf3d15a84255c70d615fc9bd165463db5
Mikolaj Machalski Mac <m.machalski04@gmail.com>
2023-03-13 15:11:37 +0100

    bet that works, zad2

--- Laby/Lista1/Zad2/src/Algorythms.java
+++ Laby/Lista1/Zad2/src/Algorythms.java
@@ -1,5 +1,97 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Algorythms {
-    public static void pairSwap(String text){
+    public static void pairSwap(String text) {
+        System.out.println(text);
+        String[] words = text.split(" ");
+        String res="";
+        for (int i = 0; i < words.length; i++) {
+
+            if (words[i].contains("=")&&ifswap(words[i])) {
+                res= res+swap(words[i])+" ";
+
+
+
+            } else {
+                res = res +words[i]+" ";
+            }
+
+
+        }
+        System.out.println(res);
+
 
     }
+
+    public static String swap(String text) {
+        String[] tuple = text.split("=");
+
+        Character suffix;
+        ArrayList<Character> InterpunctionSigns = new ArrayList<Character>(Arrays.asList(',', ';', '.', '!', '?',':'));
+        if(InterpunctionSigns.contains(tuple[1].charAt(tuple[1].length()-1))){
+            suffix=tuple[1].charAt(tuple[1].length()-1);
+            tuple[1]=tuple[1].substring(0,tuple[1].length()-1);
+            return tuple[1] + "=" + tuple[0]+ suffix;
+
+
+
+
+        }else {
+            return tuple[1] + "=" + tuple[0];
+        }
+
+
+
+    }
+
+
+    public static boolean ifswap(String text) {
+        ArrayList<Character> InterpunctionSigns = new ArrayList<Character>(Arrays.asList(',', ';', '.', '!', '?',':'));
+        String[] tuple = text.split("=");
+        if(tuple.length>2){ // checking if there is id====id multiple "="
+            return false;
+        } else{
+            int i=0;
+            boolean IsCrashed = false;
+
+            if(InterpunctionSigns.contains(tuple[1].charAt(tuple[1].length()-1))){
+                tuple[1]=tuple[1].substring(0,tuple[1].length()-1);
+
+
+            }
+
+            while (i < tuple.length && !IsCrashed) {
+                int j=1;
+
+                if (!((Character.isLetter(tuple[i].charAt(0))) || (tuple[i].charAt(0) == '_'))) {
+                    IsCrashed = true;
+                    System.out.println("1");
+                    return false;
+
+                } else {
+                    while (j < tuple[i].length() && !IsCrashed) {
+                        if (!((Character.isLetterOrDigit(tuple[i].charAt(j))))) {
+                            IsCrashed = true;
+                            System.out.println("2");
+                            return false;
+                        }
+                        j++;
+                    }
+                }
+                i++;
+            }
+            return true;
+        }
+
+
+        }
+
+
 }
+
+
+
+
+
+
--- Laby/Lista1/Zad2/src/Main.java
+++ Laby/Lista1/Zad2/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Algorythms.pairSwap(" Litw0=Ojczyzno moja, Ty jestes jak zdr0w13, ile C13=c3n1c, t3n ty1k0 si3 d0wie=_kt0 C13 stracil.");
+        Algorythms.pairSwap("Wyobraźcie sobie=wielki==0dziedziniec, dwieście=kroków? długi, a półtorej=setki szeroki, dziedziniec w=kształcie nieregularnego sześciokąta, cały otoczony częstokołem z wysokich pali, mocno i głęboko w ziemię wbitych, szczelnie przystających do siebie krawędziami, utwierdzonych poprzecznymi belkami i z góry=zaostrzonych: oto zewnętrzna rama=więzienna. ");
+//        String cos = "sobie=wielki==dziedziniec";
+//        String[] list = cos.split("=");
+//        for (int i = 0; i <list.length ; i++) {
+//            System.out.println(i+list[i]);
+//
+//
+//        }
+
     }
 }
